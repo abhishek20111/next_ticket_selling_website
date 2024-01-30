81e82a5bb74e71661487aa16faed3068b59e23f6
Whitecoder <86569824+abhishek20111@users.noreply.github.com>
2024-01-30 22:59:59 +0530

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_URI = 'mongodb://localhost:27017';
+const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = 'mongodb://localhost:27017';
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
